7027a26c08fd2d161d69311bf639595e81b9e0e9
dasibom2066 <dasibom206@gmail.com>
2026-07-21 10:06:38 +0900

    chore: 메타오토러닝 레거시 제거 및 의존성 정리

--- src/index.js
+++ src/index.js
@@ -4,36 +4,17 @@ import { HashRouter } from 'react-router-dom';
 import './common/css/reset.css';
 import './common/css/index.css';
 
-import { getCookie } from './common/js/cookie'
-
-import axios from 'axios';
-
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.withCredentials = true;
-axios.defaults.xsrfCookieName = 'csrf_access_token'
-axios.defaults.xsrfHeaderName = 'X-CSRF-TOKEN'
-axios.defaults.headers.common['X-CSRF-TOKEN'] = getCookie('csrf_access_token');
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let persistor = persistStore(store);
-
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </PersistGate>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 );
-
-reportWebVitals();
